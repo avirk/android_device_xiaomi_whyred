bfde83953b93e594fa9cbe887237f26b6b0cf275
Mohammed Javid <mjavid@codeaurora.org>
2018-10-11 15:37:20 +0530

    whyred: ipacm: Enable IPACM for SA8 target
    Enable IPACM process for SA8 target as IPA is not
    enable on this target.
    
    Change-Id: Icdc6f4b2ef5336034328ca3e55bcb5603f9f9239
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- rootdir/bin/init.class_main.sh
+++ rootdir/bin/init.class_main.sh
@@ -1,6 +1,6 @@
 #! /vendor/bin/sh
 
-# Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+# Copyright (c) 2013-2018, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -44,6 +44,11 @@ case "$baseband" in
       stop vendor.ril-daemon
       start vendor.ipacm
     fi
+esac
+
+case "$baseband" in
+    "sa8")
+    start vendor.ipacm
 esac
 
 case "$baseband" in
