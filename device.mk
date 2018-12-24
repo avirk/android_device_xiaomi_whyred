a02d38800dfc4d8145688b274a660d4b244de474
dianlujitao <dianlujitao@lineageos.org>
2018-12-24 12:04:48 +0800

    whyred: Support Alipay fingerprint payment
     * IFAA manager is based on OnePlusOSS, but adapted for Xiaomi's mlipay
       interface.
     * vendor.xiaomi.hardware.mtdservice@1.0.so is not actually used, thus
       patchelf to drop it rather than shipping a blob.
    
    Change-Id: Idf3d3a8d40245984767f4ef5f60f9fe584e69f21
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- device.mk
+++ device.mk
@@ -80,6 +80,13 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# Alipay
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl:32 \
