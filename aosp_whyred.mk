88ab20d7520de71227490d0d6a5e5ab31c72820b
Shubham Singh <coolsks94@gmail.com>
2019-03-30 00:04:47 +0100

    Add support to build both gapps and non-gapps build
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -48,7 +48,11 @@ BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIF
 
 TARGET_VENDOR := Xiaomi
 
+# Set this flag in build script
+ifeq ($(CURRENT_BUILD_TYPE), gapps)
 # Use Gapps
+TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
 WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
 IS_PHONE := true
+endif
